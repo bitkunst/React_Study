28117b3beb78afc121cf755ebb0726c2df9dc7cc
bitkunst <bitkunst23@gmail.com>
2022-05-10 08:46:05 +0900

    220509 study

--- 220506/middleware/src/sagas/index.js
+++ 220506/middleware/src/sagas/index.js
@@ -1,6 +1,8 @@
 import { all } from 'redux-saga/effects'
 import watchCounter from './counterSaga.js'
 
+console.log(all)
+
 export default function* rootSaga() {
     yield all([
         watchCounter()
